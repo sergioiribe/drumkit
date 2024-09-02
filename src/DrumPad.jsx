6b5dc4ee0a07d4e58b0c3749de36773f3e57ac12
sergio iribe <sergio.iribec@gmail.com>
2024-09-02 15:18:51 -0700

    fix screen 9

--- src/DrumPad.jsx
+++ src/DrumPad.jsx
@@ -21,7 +21,7 @@ export const DrumPad = ({letter,sound, keyCode, handlePadClick}) => {
   }
 
   return (
-    <div data-key={keyCode}  className='key p-2 w-[100px] md:p-3  md:w-[100px] md:h-[100px] border-2 border-blue-600 rounded justify-center items-center flex flex-col z-10 bg-yellow-300 opacity-2 transfrom cursor-pointer transition-transform duration-100' onClick={() => playSound(keyCode)}>
+    <div data-key={keyCode}  className='key p-2 w-[100px] md:p-3  md:w-[100px] md:h-[100px] border-2 border-blue-600 rounded justify-center items-center flex flex-col z-10 bg-yellow-300 opacity-2 transfrom cursor-pointer transition-transform duration-100 overflow-hidden' onClick={() => playSound(keyCode)}>
       <kbd className='text-red-400 flex justify-center font-bold text-4xl'>{letter}</kbd>
       <p className='text-blue-400 flex justify-center font-semibold'>{sound}</p>
     </div>
